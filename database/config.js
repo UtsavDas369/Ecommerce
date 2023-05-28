8ec49c3265855eef2d22ab91060cfceb32032043
Utsav Das <85437460+UtsavDas369@users.noreply.github.com>
2023-05-28 18:19:13 +0530

    Revert "Update config.js"
    This reverts commit 7216e5300cd14bc10ad6a7d1685f2f63e6f1bd1a.

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   port     : '3306',
   user     : 'root',
-  password : 'password',
+  password : '3005',
   //socket   : '/Applications/MAMP/tmp/mysql/mysql.sock',
   database : 'ecommerce'
 });
